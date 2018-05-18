19d46c285c7fed9a40509aa68bf41241a84853fd
Haibin <buaanusyu@gmail.com>
2018-05-18 16:54:14 +0800

    200 Number of Islands

---
+++ 200-number-of-islands/README.md
@@ -0,0 +1,9 @@
+## Idea
+
+Same idea to [695 Max Area of Island](https://leetcode.com/problems/max-area-of-island/description/). 
+
+when 
+```
+self.maxArea(m, n, grid) != 0
+result += 1
+```
---
+++ 200-number-of-islands/solution.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid: return 0
+        self.m = len(grid)
+        self.n = len(grid[0])
+        result = 0
+        self.seen = set()
+        max_area = 0
+        for m in range(self.m):
+            for n in range(self.n):
+                if self.maxArea(m, n, grid):
+                    result += 1
+        return result
+        
+        
+    def maxArea(self, i, j, grid):
+        if (i, j) in self.seen or grid[i][j] == '0':
+            return 0
+        self.seen.add((i, j))
+        up = 0 if i == 0 else self.maxArea(i-1, j, grid)
+        down = 0 if i == self.m - 1 else self.maxArea(i+1, j, grid)
+        left = 0 if j == 0 else self.maxArea(i, j-1, grid) 
+        right = 0 if j == self.n - 1 else self.maxArea(i, j+1, grid)
+        return 1 + up + down + left + right
